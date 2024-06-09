59232c353d0da10294c2b4755bbb6d47e1c1cef2
Bifdus <denimhodgson@gmail.com>
2024-06-10 00:11:09 +0800

    Added buffer section to which-key
    Added buffer section to which key for easier buffer navigation

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -22,6 +22,7 @@ return {
 
       -- Document existing key chains
       require('which-key').register {
+        ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
