72bb92bccca6f85fa24737b2c7c2b493df9ac12d
Bifdus <denimhodgson@gmail.com>
2024-08-03 14:28:53 +0800

    Added json and c++ to treesitter

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'python', 'php', 'c_sharp', 'sql' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'python', 'php', 'c_sharp', 'sql', 'cpp', 'json' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
