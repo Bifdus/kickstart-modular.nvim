d33ce6dd9a87651644d7025c6c91327910aa9db2
Bifdus <denimhodgson@gmail.com>
2024-08-11 22:15:17 +0800

    Configuring flash nvim

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -3,11 +3,63 @@ return {
     'folke/flash.nvim',
     event = 'VeryLazy',
     ---@type Flash.Config
-    opts = {},
+    opts = {
+      modes = {
+        char = {
+          enabled = true,
+          char_actions = function(motion)
+            return {
+              [','] = 'next',
+              [';'] = 'prev',
+              [motion:lower()] = 'next',
+              [motion:upper()] = 'prev',
+            }
+          end,
+        },
+      },
+    },
     -- stylua: ignore
     keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+      {
+        'S',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').treesitter_search()
+        end,
+        desc = 'Flash Treesitter',
+      },
+      {
+        'r',
+        mode = 'o',
+        function()
+          require('flash').remote()
+        end,
+        desc = 'Remote Flash',
+      },
+      {
+        'R',
+        mode = { 'o', 'x' },
+        function()
+          require('flash').treesitter_search()
+        end,
+        desc = 'Treesitter Search',
+      },
+      {
+        '<leader>tf',
+        mode = { 'c' },
+        function()
+          require('flash').toggle()
+        end,
+        desc = 'Toggle Flash Search',
+      },
     },
   },
 }
