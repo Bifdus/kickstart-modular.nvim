d2f24de99116dad017ef8d13d4d9e2b879faf62d
Bifdus <denimhodgson@gmail.com>
2024-08-26 21:59:37 +0800

    Treesitter refactor, moved devdocs to coding plugin file

--- lua/custom/plugins/coding.lua
+++ lua/custom/plugins/coding.lua
@@ -135,6 +135,20 @@ return {
       vim.g.vimtex_view_method = 'zathura'
     end,
   },
+  -- Devdocs
+  {
+    'luckasRanarison/nvim-devdocs',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    keys = {
+      -- { '<leader>sa', '<cmd>DevdocsOpen<cr>', desc = 'Devdocs' },
+      { '<leader>sa', '<cmd>DevdocsOpenCurrent<cr>', desc = 'Devdocs Current' },
+    },
+    opts = {},
+  },
   -- Improved yanking
   {
     'gbprod/yanky.nvim',
--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -2,6 +2,12 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    dependencies = {
+      'windwp/nvim-ts-autotag',
+      'nvim-treesitter/playground',
+      'HiPhish/rainbow-delimiters.nvim',
+      { 'nvim-treesitter/nvim-treesitter-context', lazy = true, event = 'VeryLazy' },
+    },
     opts = {
       ensure_installed = {
         'bash',
@@ -26,7 +32,6 @@ return {
         'regex',
         'latex',
       },
-      -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
         enable = true,
@@ -37,7 +42,31 @@ return {
       },
       fold = { true },
       indent = { enable = true, disable = { 'ruby' } },
-      dependencies = { 'windwp/nvim-ts-autotag' },
+      -- Additional Plugins
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25,
+        persist_queries = false,
+        keybindings = {
+          toggle_query_editor = 'o',
+          toggle_hl_groups = 'i',
+          toggle_injected_languages = 't',
+          toggle_anonymous_nodes = 'a',
+          toggle_language_display = 'I',
+          focus_language = 'f',
+          unfocus_language = 'F',
+          update = 'R',
+          goto_node = '<cr>',
+          show_help = '?',
+        },
+      },
+      context = {
+        enable = true,
+        max_lines = 15,
+        trim_scope = 'inner',
+      },
+      -- Autoinstall languages that are not installed
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -46,46 +75,12 @@ return {
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
       --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-    end,
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    event = 'VeryLazy',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', lazy = true },
-    config = function()
-      require('treesitter-context').setup {
-        max_lines = 15,
-        trim_scope = 'inner',
-        -- multiline_threshold = 1,
-        -- throttle = true,
-      }
-
       vim.keymap.set('n', '<leader>tc', '<CMD>TSContextToggle<CR>', { desc = '[t]oggle treesitter [c]ontext', silent = true })
+      vim.keymap.set('n', '<leader>tH', '<CMD>TSPlaygroundToggle<CR>', { desc = '[t]oggle treesitter playground [h]ighlight groups', silent = true })
     end,
-  },
-  {
-    'HiPhish/rainbow-delimiters.nvim',
-    event = 'VeryLazy',
-  },
-  {
-    'luckasRanarison/nvim-devdocs',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    keys = {
-      -- { '<leader>sa', '<cmd>DevdocsOpen<cr>', desc = 'Devdocs' },
-      { '<leader>sa', '<cmd>DevdocsOpenCurrent<cr>', desc = 'Devdocs Current' },
-    },
-    opts = {},
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
