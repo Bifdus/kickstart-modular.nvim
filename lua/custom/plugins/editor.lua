15f98dde41de5bab2a9d9e1c42f49bf986a7a840
Bifdus <denimhodgson@gmail.com>
2024-09-05 23:20:10 +0800

    CCC for colors

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -138,41 +138,15 @@ return {
       end, { desc = 'Harpoon: 4' })
     end,
   },
+
   -- Returns you to last place in file when reopening
   { 'ethanholz/nvim-lastplace', opts = {} },
   {
-    'Bifdus/nvim-highlight-colors',
-    branch = 'shadcn-css-vars', -- This is the regexp branch, use this for the new version
+    'uga-rosa/ccc.nvim',
     config = function()
-      require('nvim-highlight-colors').setup {
-        ---Render style
-        ---@usage 'background'|'foreground'|'virtual'
-        render = 'background',
-        ---Set virtual symbol (requires render to be set to 'virtual')
-        virtual_symbol = '■',
-        ---Set virtual symbol suffix (defaults to '')
-        virtual_symbol_prefix = '',
-        virtual_symbol_suffix = ' ',
-        virtual_symbol_position = 'inline',
-        ---Highlight hex colors, e.g. '#FFFFFF'
-        enable_hex = true,
-        ---Highlight short hex colors e.g. '#fff'
-        enable_short_hex = true,
-        ---Highlight rgb colors, e.g. 'rgb(0 0 0)'
-        enable_rgb = true,
-        ---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
-        enable_hsl = true,
-        ---Highlight CSS variables, e.g. 'var(--testing-color)'
-        enable_var_usage = true,
-        ---Highlight named colors, e.g. 'green'
-        enable_named_colors = true,
-        ---Highlight tailwind colors, e.g. 'bg-blue-500'
-        enable_tailwind = true,
-        ---Set custom colors
-        ---Label must be properly escaped with '%' to adhere to `string.gmatch`
-        --- :help string.gmatch
-        exclude_filetypes = { 'php', 'python', 'c', 'cpp' },
-        exclude_buftypes = { 'terminal', 'nofile', 'quickfix', 'prompt' },
+      require('ccc').setup {
+        auto_enable = true,
+        lsp = true
       }
     end,
   },
