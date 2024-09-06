71c613138703147de26fd7f2f7607cba5c008f0a
Bifdus <denimhodgson@gmail.com>
2024-09-06 22:30:35 +0800

    Adding LspSaga for better LSP, configured to work with TStools as well

--- lua/custom/plugins/coding.lua
+++ lua/custom/plugins/coding.lua
@@ -57,8 +57,8 @@ return {
         function()
           require('precognition').peek()
         end,
+        desc = '[p]rerecognition [p]eek',
       },
-      desc = '[p]rerecognition [p]eek',
     },
   },
   -- Diagnostics
@@ -105,7 +105,72 @@ return {
   {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {},
+    opts = {
+      on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+
+        if client.server_capabilities.documentSymbolProvider then
+          require('nvim-navic').attach(client, bufnr)
+        end
+
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set('n', '<leader>ttr', '<cmd>TSToolsRenameFile<CR>', opts)
+        vim.keymap.set('n', '<leader>tti', '<cmd>TSToolsOrganizeImports<CR>', opts)
+        vim.keymap.set('n', '<leader>tto', '<cmd>TSToolsSortImports<CR>', opts)
+        vim.keymap.set('n', '<leader>tta', '<cmd>TSToolsAddMissingImports<CR>', opts)
+      end,
+    },
+    config = function(_, opts)
+      require('typescript-tools').setup(opts)
+    end,
+  },
+  -- Lsp Saga
+  {
+    'nvimdev/lspsaga.nvim',
+    opts = {
+      lightbulb = {
+        enable = false,
+        enable_in_insert = false,
+        sign = false,
+        sign_priority = 40,
+        virtual_text = false,
+      },
+      finder = {
+        max_height = 0.5,
+        min_width = 30,
+        force_max_height = false,
+        keys = {
+          jump_to = 'p',
+          expand_or_jump = 'o',
+          vsplit = 's',
+          split = 'i',
+          tabe = 't',
+          quit = { 'q', '<ESC>' },
+        },
+      },
+      definition = {
+        edit = '<C-c>o',
+        vsplit = '<C-c>v',
+        split = '<C-c>i',
+        tabe = '<C-c>t',
+        quit = 'q',
+      },
+      code_action = {
+        num_shortcut = true,
+        keys = {
+          quit = 'q',
+          exec = '<CR>',
+        },
+      },
+    },
+    config = function(_, opts)
+      require('lspsaga').setup(opts)
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter', -- optional
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
   },
   -- Python venv selector
   {
@@ -189,5 +254,86 @@ return {
     config = function()
       require('nvim-ts-autotag').setup()
     end,
+  },
+
+  -- VS Code like winbar
+  {
+    'utilyre/barbecue.nvim',
+    dependencies = { 'SmiteshP/nvim-navic', 'nvim-tree/nvim-web-devicons' },
+    keys = {
+      {
+        '<Leader>tB',
+        function()
+          local off = vim.b['barbecue_entries'] == nil
+          require('barbecue.ui').toggle(off and true or nil)
+        end,
+        desc = 'Breadcrumbs toggle',
+      },
+    },
+    opts = {
+      enable = true,
+      attach_navic = true,
+      show_dirname = false,
+      show_modified = true,
+      -- kinds = kind_icons, -- Uncomment if you have kind_icons defined
+      symbols = { separator = '' },
+    },
+    config = function(_, opts)
+      require('barbecue').setup(opts)
+    end,
+  },
+  {
+    'folke/zen-mode.nvim',
+    config = function()
+      require('zen-mode').setup {
+        window = {
+          backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+          width = 0.8, -- width of the Zen window
+          height = 1, -- height of the Zen window
+          -- by default, no options are changed for the Zen window
+          -- uncomment any of the options below, or add other vim.wo options you want to apply
+          options = {
+            -- signcolumn = "no", -- disable signcolumn
+            -- number = false, -- disable number column
+            -- relativenumber = false, -- disable relative numbers
+            -- cursorline = false, -- disable cursorline
+            -- cursorcolumn = false, -- disable cursor column
+            -- foldcolumn = "0", -- disable fold column
+            -- list = false, -- disable whitespace characters
+          },
+        },
+        plugins = {
+          -- disable some global vim options (vim.o...)
+          -- comment the lines to not apply the options
+          options = {
+            enabled = true,
+            ruler = false, -- disables the ruler text in the cmd line area
+            showcmd = false, -- disables the command in the last line of the screen
+          },
+          gitsigns = { enabled = true }, -- disables git signs
+          -- alacritty = {
+          --   enabled = EcoVim.plugins.zen.alacritty_enabled or false,
+          --   font = '14', -- font size
+          -- },
+        },
+        -- callback where you can add custom code when the Zen window opens
+        on_open = function()
+          require('gitsigns.actions').toggle_current_line_blame()
+          vim.cmd 'IBLDisable'
+          vim.opt.relativenumber = false
+          vim.opt.signcolumn = 'no'
+          require('gitsigns.actions').refresh()
+        end,
+        -- callback where you can add custom code when the Zen window closes
+        on_close = function()
+          require('gitsigns.actions').toggle_current_line_blame()
+          vim.cmd 'IBLEnable'
+          vim.opt.relativenumber = true
+          vim.opt.signcolumn = 'yes:2'
+          require('gitsigns.actions').refresh()
+        end,
+      }
+    end,
+    keys = { { '<leader>tz', '<cmd>ZenMode<cr>', desc = 'Zen Mode' } },
   },
 }
--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -79,35 +79,6 @@ return {
       extensions = {},
     },
   },
-  -- Tabs
-  {
-    'akinsho/bufferline.nvim',
-    event = 'VeryLazy',
-    keys = {
-      { '<leader>bn', '<Cmd>BufferLineCycleNext<CR>', desc = 'Next tab' },
-      { '<leader>bb', '<Cmd>BufferLineCyclePrev<CR>', desc = 'Prev tab' },
-    },
-    opts = {
-      options = {
-        mode = 'tabs',
-        debug = {
-          logging = true,
-        },
-        version = '*',
-        enforce_regular_tabs = false,
-        always_show_bufferline = true,
-        offsets = {
-          {
-            filetype = 'neo-tree',
-            text = 'File Explorer',
-            highlight = 'Directory',
-            separator = true,
-          },
-        },
-        dependencies = 'nvim-tree-web-devicons',
-      },
-    },
-  },
   -- UI Replacement for messages, cmdline and popup menu
   {
     'folke/noice.nvim',
