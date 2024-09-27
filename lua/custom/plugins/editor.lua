38457d6cac064b26d40eb621d045cf50fbe123ce
Bifdus <denimhodgson@gmail.com>
2024-09-27 15:44:00 +0800

    Org mode for note taking and task management

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -400,4 +400,26 @@ return {
       vim.api.nvim_set_hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
     end,
   },
+
+  -----------------------------------------------------------------------------
+  -- Note taking and todo list
+  {
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    config = function()
+      -- Setup orgmode
+      require('orgmode').setup {
+        org_agenda_files = '~/orgfiles/**/*',
+        org_default_notes_file = '~/orgfiles/refile.org',
+      }
+
+      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+      -- add ~org~ to ignore_install
+      -- require('nvim-treesitter.configs').setup({
+      --   ensure_installed = 'all',
+      --   ignore_install = { 'org' },
+      -- })
+    end,
+  },
 }
