845417761bc5d738a76868d99d2734ee96660f4b
Bifdus <denimhodgson@gmail.com>
2024-09-28 15:27:40 +0800

    refactor: lualine config using cyberdream

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -49,38 +49,74 @@ return {
   -- Statusline
   {
     'nvim-lualine/lualine.nvim',
-    event = 'VeryLazy',
-    opts = {
-      icons_enabled = true,
-      theme = 'auto', -- you can choose a specific theme here
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
+    lazy = false,
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = function()
+      local utils = require 'custom.functions.utils'
 
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
-        lualine_c = {
-          {
-            'filename',
-            file_status = true,
-            path = 3,
-          },
+      return {
+        options = {
+          component_separators = { left = ' ', right = ' ' },
+          section_separators = { left = ' ', right = ' ' },
+          theme = 'cyberdream',
+          globalstatus = true,
+          disabled_filetypes = { statusline = { 'dashboard', 'alpha' } },
         },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      extensions = {},
-    },
+        sections = {
+          lualine_a = { { 'mode', icon = '' } },
+          lualine_b = {
+            { 'branch', icon = '' },
+          },
+          lualine_c = {
+            { 'filetype', icon_only = true, separator = '', padding = { left = 1, right = 0 } },
+            {
+              'filename',
+              padding = { left = 1, right = 0 },
+              file_status = true, -- Displays file status (readonly status, modified status)
+              newfile_status = false, -- Display new file status (new file means no write after created)
+              path = 1, -- 0: Just the filename
+              -- 1: Relative path
+              -- 2: Absolute path
+              -- 3: Absolute path, with tilde as the home directory
+              -- 4: Filename and parent dir, with tilde as the home directory
+
+              shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+              -- for other components. (terrible name, any suggestions?)
+              symbols = {
+                modified = '[+]', -- Text to show when the file is modified.
+                readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+                unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                newfile = '[New]', -- Text to show for newly created file before first write
+              },
+            },
+          },
+          lualine_x = {
+            {
+              'diagnostics',
+              always_visible = false,
+              symbols = {
+                error = ' ',
+                warn = ' ',
+                info = ' ',
+                hint = '󰝶 ',
+              },
+            },
+            { 'diff' },
+          },
+          lualine_y = {
+            {
+              'progress',
+            },
+            {
+              'location',
+              color = utils.get_hlgroup 'Boolean',
+            },
+          },
+          lualine_z = {},
+        },
+        extensions = { 'lazy', 'toggleterm', 'mason', 'neo-tree', 'trouble' },
+      }
+    end,
   },
 
   -----------------------------------------------------------------------------
