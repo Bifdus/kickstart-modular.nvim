d2a91c6f7162f71636e7ec13c14e3c0f2507ecb6
Bifdus <denimhodgson@gmail.com>
2024-10-14 21:23:29 +0800

    fix: fixed lazy load of telescope
    lazy.nvim plugin spec for keymaps

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -1,7 +1,27 @@
 return {
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
+    lazy = true,
     cmd = 'Telescope',
+    keys = {
+      { '<leader>sh', '<cmd>Telescope help_tags<cr>', desc = '[S]earch [H]elp' },
+      { '<leader>sk', '<cmd>Telescope keymaps<cr>', desc = '[S]earch [K]eymaps' },
+      { '<leader>sf', '<cmd>Telescope find_files<cr>', desc = '[S]earch [F]iles' },
+      { '<leader>st', '<cmd>Telescope builtin<cr>', desc = '[S]earch [S]elect Telescope' },
+      { '<leader>scw', '<cmd>Telescope grep_string<cr>', desc = '[S]earch current [W]ord' },
+      { '<leader>sg', '<cmd>Telescope live_grep<cr>', desc = '[S]earch by [G]rep' },
+      { '<leader>sd', '<cmd>Telescope diagnostics<cr>', desc = '[S]earch [D]iagnostics' },
+      { '<leader>sR', '<cmd>Telescope resume<cr>', desc = '[S]earch [R]esume' },
+      { '<leader>sr', '<cmd>Telescope lsp_references<cr>', desc = '[S]earch [r]eferences' },
+      { '<leader>sI', '<cmd>Telescope lsp_incoming_calls<cr>', desc = '[S]earch [i]ncoming calls' },
+      { '<leader>sO', '<cmd>Telescope lsp_outgoing_calls<cr>', desc = '[S]earch [o]utgoing calls' },
+      { '<leader>s.', '<cmd>Telescope oldfiles<cr>', desc = '[S]earch Recent Files ("." for repeat)' },
+      { '<leader>sb', '<cmd>Telescope buffers<cr>', desc = '[S]earch existing buffers' },
+      { '<leader>ss', '<cmd>Telescope lsp_document_symbols<cr>', desc = '[S]earch [S]ymbols' },
+      { '<leader>sj', '<cmd>Telescope jumplist<cr>', desc = '[S]earch [J]umps' },
+      { '<leader>su', '<cmd>Telescope undo<cr>', desc = '[S]earch [U]ndo' },
+      { '<leader>sj', '<cmd>Telescope jsonfly<cr>', desc = '[s]earch [j]son' },
+    },
     -- event = 'VimEnter',
     branch = '0.1.x',
     dependencies = {
@@ -18,7 +38,6 @@ return {
       { 'nvim-telescope/telescope-ui-select.nvim' },
       {
         'nvim-orgmode/telescope-orgmode.nvim',
-        -- event = 'VeryLazy',
         after = { 'telescope.nvim', 'orgmode' },
         dependencies = {
           'nvim-orgmode/orgmode',
@@ -114,26 +133,7 @@ return {
       pcall(require('telescope').load_extension, 'undo')
       pcall(require('telescope').load_extension 'textcase')
 
-      -- See `:help telescope.builtin`
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>st', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>scw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sR', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>sr', builtin.lsp_references, { desc = '[S]earch [r]eferences' })
-      vim.keymap.set('n', '<leader>sI', builtin.lsp_incoming_calls, { desc = '[S]earch [i]ncoming calls' })
-      vim.keymap.set('n', '<leader>sO', builtin.lsp_outgoing_calls, { desc = '[S]earch [o]utgoing calls' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch existing buffers' })
-      vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, { desc = '[S]earch [S]ymbols' })
-      vim.keymap.set('n', '<leader>sj', builtin.jumplist, { desc = '[S]earch [J]umps' })
-      vim.keymap.set('n', '<leader>su', '<cmd>Telescope undo<cr>')
-      vim.keymap.set('n', '<leader>sj', '<cmd>Telescope jsonfly<cr>', { desc = '[s]earch [j]son' })
-
+      -- Text Case Mappings
       vim.api.nvim_set_keymap('n', 'gu.', '<cmd>TextCaseOpenTelescope<CR>', { desc = 'Telescope' })
       vim.api.nvim_set_keymap('v', 'gu.', '<cmd>TextCaseOpenTelescope<CR>', { desc = 'Telescope' })
 
