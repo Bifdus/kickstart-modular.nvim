6d69ab95c408142afe377b5a99aec3f0de629b3c
Bifdus <denimhodgson@gmail.com>
2025-02-09 00:03:22 +0800

    adding mappings for scratch

--- lua/plugins/snacks/core.lua
+++ lua/plugins/snacks/core.lua
@@ -17,4 +17,10 @@ return {
     },
     profile = { enabled = true },
   },
+  --stylua: ignore
+  keys = {
+    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+
+  },
 }
