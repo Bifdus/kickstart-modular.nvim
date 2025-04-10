14422ef844d91c81dd9f605b4fd48ee8c4064436
Bifdus <denimhodgson@gmail.com>
2025-04-10 17:51:42 +0800

    csvview plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -750,4 +750,32 @@ return {
       require('focus').setup()
     end,
   },
+  {
+    'hat0uma/csvview.nvim',
+    opts = {
+      parser = { comments = { '#', '//' } },
+      view = {
+        display_mode = 'border',
+        header_lnum = 1,
+      },
+      keymaps = {
+        textobject_field_inner = { 'if', mode = { 'o', 'x' } },
+        textobject_field_outer = { 'af', mode = { 'o', 'x' } },
+        jump_next_field_end = { '<Tab>', mode = { 'n', 'v' } },
+        jump_prev_field_end = { '<S-Tab>', mode = { 'n', 'v' } },
+        jump_next_row = { '<Enter>', mode = { 'n', 'v' } },
+        jump_prev_row = { '<S-Enter>', mode = { 'n', 'v' } },
+      },
+    },
+    config = function(_, opts)
+      require('csvview').setup(opts)
+      vim.keymap.set('n', '<leader>uv', function()
+        require('csvview').toggle()
+        if require('csvview').is_enabled(0) then
+          vim.opt_local.wrap = false
+        end
+      end, { desc = 'Toggle CSV view' })
+    end,
+    cmd = { 'CsvViewEnable', 'CsvViewDisable', 'CsvViewToggle' },
+  },
 }
