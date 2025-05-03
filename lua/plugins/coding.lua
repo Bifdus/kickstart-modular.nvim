df602bc89a1f9aee88842284e3dbac15df17ec41
Bifdus <denimhodgson@gmail.com>
2025-05-03 14:13:42 +0800

    Added tiny inline diagnostic for better virtual text

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -547,4 +547,13 @@ return {
       require('exercism').setup(opts)
     end,
   },
+  {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy',
+    priority = 1000,
+    config = function()
+      require('tiny-inline-diagnostic').setup()
+      vim.diagnostic.config { virtual_text = false, virtual_lines = false }
+    end,
+  },
 }
