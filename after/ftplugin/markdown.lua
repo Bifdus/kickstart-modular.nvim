33555a231adc8caa1b7411746c85102946b8c271
Bifdus <denimhodgson@gmail.com>
2025-05-28 23:51:31 +0800

    ftplugin setup for markdown folds

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------
+-- Markdown Folds
+
+-- Use <CR> to fold when in normal mode
+-- To see help about folds use `:help fold`
+vim.keymap.set("n", "<CR>", function()
+  -- Get the current line number
+  local line = vim.fn.line(".")
+  -- Get the fold level of the current line
+  local foldlevel = vim.fn.foldlevel(line)
+  if foldlevel == 0 then
+    vim.notify("No fold found", vim.log.levels.INFO)
+  else
+    vim.cmd("normal! za")
+    vim.cmd("normal! zz") -- center the cursor line on screen
+  end
+end, { desc = "[P]Toggle fold" })
+
+-----------------------------------------------------------------------------
